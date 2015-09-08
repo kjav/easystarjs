e4f6c57898765e715d3cc81f43ba4f9682408d83
kjav <sam.rr.lee@gmail.com>
2015-09-08 16:19:40 +0100

    Change diagonal cost

--- src/easystar.js
+++ src/easystar.js
@@ -7,7 +7,7 @@
 **/
 EasyStar.js = function() {
 	var STRAIGHT_COST = 1.0;
-	var DIAGONAL_COST = 1.4;
+	var DIAGONAL_COST = 1.4142135623730951;
 	var syncEnabled = false;
 	var pointsToAvoid = {};
 	var collisionGrid;
